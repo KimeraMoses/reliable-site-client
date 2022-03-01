5c9cd6bab66833ebd550d017942e0a996736cb4d
Usama Tufail <miansamusama@gmail.com>
2022-03-01 20:54:43 +0500

    feat(editor): separated editor into a component

--- src/pages/Dashboard/Editor/Editor.page.jsx
+++ src/pages/Dashboard/Editor/Editor.page.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import { EditorState } from 'draft-js';
-import { Editor } from 'react-draft-wysiwyg';
+import { CustomEditor } from 'components';
 import { DashboardLayout } from 'layout';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import './Editor.styles.scss';
 
 function EditorPage() {
@@ -17,57 +16,12 @@ function EditorPage() {
       <div className="ep">
         <h1 className="ep__heading">WYSIWYG Editor</h1>
         <div className="ep__container">
-          <Editor
+          <CustomEditor
             editorState={editorState}
             wrapperClassName="ep__container-wrapper"
             editorClassName="ep__container-editor"
-            onEditorStateChange={onEditorStateChange}
+            onChange={onEditorStateChange}
             placeholder="Start typing here..."
-            toolbar={{
-              options: ['inline', 'textAlign', 'link'],
-              inline: {
-                options: ['bold', 'italic', 'underline'],
-                bold: {
-                  icon: '/icon/dashboard/bold.svg',
-                  className: 'demo-option-custom',
-                },
-                italic: {
-                  icon: '/icon/dashboard/italic.svg',
-                  className: 'demo-option-custom',
-                },
-                underline: {
-                  icon: '/icon/dashboard/underline.svg',
-                  className: 'demo-option-custom',
-                },
-              },
-              textAlign: {
-                left: {
-                  icon: '/icon/dashboard/left.svg',
-                  className: 'demo-option-custom',
-                },
-                center: {
-                  icon: '/icon/dashboard/center.svg',
-                  className: 'demo-option-custom',
-                },
-                right: {
-                  icon: '/icon/dashboard/right.svg',
-                  className: 'demo-option-custom',
-                },
-                justify: {
-                  icon: '/icon/dashboard/justify.svg',
-                  className: 'demo-option-custom',
-                },
-              },
-              link: {
-                options: ['link'],
-                popupClassName: 'ep__container-link',
-                link: {
-                  icon: '/icon/dashboard/link.svg',
-                  className: 'demo-option-custom',
-                },
-                // unlink: { icon: Icons.unlink, className: 'demo-option-custom' },
-              },
-            }}
           />
         </div>
       </div>
